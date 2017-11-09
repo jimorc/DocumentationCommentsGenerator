5a266838b8f7ff4d5a310c7bd9f7bc40a9413c32
Jim <jimorcheson@yahoo.ca>
2017-11-09 12:28:36 -0500

    Create barebones documentation comments generator.
    The framework for a documentation comments generator has been created as follows:
    
    New VSIX project created. Custom comment GenerateDocumentationComments added. DocumentCommentsRewriter class created with override of CSharpSyntaxRewriter.VisitClassDeclaration method added. All this method does is call the base class's VisitClassDeclaration.

---
+++ DocumentationCommentsGenerator/DocumentCommentsRewriter.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Runtime.CompilerServices;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace DocumentationCommentsGenerator
+{
+    internal class DocumentCommentsRewriter : CSharpSyntaxRewriter
+    {
+        public override SyntaxNode VisitClassDeclaration(ClassDeclarationSyntax node)
+        {
+            return base.VisitClassDeclaration(node);
+        }
+    }
+}
---
+++ DocumentationCommentsGenerator/GenerateCustomComments.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.ComponentModel.Design;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Shell;
+
+namespace DocumentationCommentsGenerator
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class GenerateCustomComments
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("5d406879-b208-42fc-855c-529f129efcce");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenerateCustomComments"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private GenerateCustomComments(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static GenerateCustomComments Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new GenerateCustomComments(package);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            var textView = GetTextViewOfActiveDocument();
+            if (textView != null)
+            {
+                var code = textView.TextSnapshot.GetText();
+                var syntaxTree = CSharpSyntaxTree.ParseText(code);
+                var root = syntaxTree.GetRoot();
+                var rewriter = new DocumentCommentsRewriter();
+                var newCode = rewriter.Visit(root).ToFullString();
+                textView.TextBuffer.Replace(new Span(0, textView.TextSnapshot.Length),
+                    newCode);
+            }
+        }
+
+        private ITextView GetTextViewOfActiveDocument()
+        {
+            var componentModel = (IComponentModel)Package.GetGlobalService(typeof(SComponentModel));
+            var textManager = (IVsTextManager)Package.GetGlobalService(typeof(SVsTextManager));
+            IVsTextView activeView = null;
+            ErrorHandler.ThrowOnFailure(textManager.GetActiveView(1, null, out activeView));
+            var editorAdapter = componentModel.GetService<IVsEditorAdaptersFactoryService>();
+            var textView = editorAdapter.GetWpfTextView(activeView);
+            return (textView.TextBuffer.ContentType.TypeName.Equals("CSharp"))
+                ? textView : null;
+        }
+    }
+}
