32281ad15d6a9d7f5291e7fb41fcd207197febe8
Jim <jimorcheson@yahoo.ca>
2017-11-10 08:01:54 -0500

    Add summary comment for internal class declaration.
    A test was added for this. No code changes were required to satisfy this test because code to do so had already been copied from a previous incarnation of this project.

--- DocumentationCommentsGeneratorTests/ClassDeclarationTests.cs
+++ DocumentationCommentsGeneratorTests/ClassDeclarationTests.cs
@@ -74,6 +74,28 @@ namespace GenerateDocumentationCommentsTests
 
             Assert.Equal(expected, result.ToFullString());
         }
-    }
+        [Fact]
+        public void ShouldAddSummaryDocCommentsToInternalClassDeclaration()
+        {
+            var classDecl =
+@"    internal class Class1
+    {
+    }";
+            var expected =
+@"    /// <summary>
+    /// 
+    /// </summary>
+    internal class Class1
+    {
+    }";
+            var tree = CSharpSyntaxTree.ParseText(classDecl);
+            var rewriter = new DocumentCommentsRewriter();
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+            var classDeclSyntax = (ClassDeclarationSyntax)root.Members[0];
 
+            var result = rewriter.VisitClassDeclaration(classDeclSyntax);
+
+            Assert.Equal(expected, result.ToFullString());
+        }
+    }
 }
