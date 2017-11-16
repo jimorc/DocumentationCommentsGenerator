622bc6725bf3153c93d07a491068d1314357d5e3
Jim <jimorcheson@yahoo.ca>
2017-11-16 12:27:42 -0500

    Remove unnecessary using directives.

--- DocumentationCommentsGenerator/ClassDeclarationDocumentationComments.cs
+++ DocumentationCommentsGenerator/ClassDeclarationDocumentationComments.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
--- DocumentationCommentsGenerator/DocumentationComments.cs
+++ DocumentationCommentsGenerator/DocumentationComments.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
